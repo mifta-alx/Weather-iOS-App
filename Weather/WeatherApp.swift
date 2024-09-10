3c77cdc00ba1ebcc006368a07c1f0c5764eef16b
mifta-alx <alx.yoi67@gmail.com>
2024-09-10 20:43:37 +0700

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Alx on 10/09/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
